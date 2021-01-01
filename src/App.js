30c57069446457dd378e9534e1286df0da812a83
hangnguyen1199 <nthang1811@gmail.com>
2021-01-01 23:22:12 +0700

    5

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShoppingList from "./shoppingList";
 function App() {
   return(
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <Switch>
           <Route path="/shopping-list" exact component={ShoppingList} />
           <Route path="/shopping-list/share" component={Share} />
