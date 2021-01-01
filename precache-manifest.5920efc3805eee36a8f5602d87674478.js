8e83a2b0ac898355ccda8bd5aee6ae908660dd66
hangnguyen1199 <nthang1811@gmail.com>
2021-01-01 22:43:01 +0700

    Updates

---
+++ precache-manifest.5920efc3805eee36a8f5602d87674478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72096797ba0ef90bb7d71bfcb77357d4",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "d81ada1cfa8641d4666a",
+    "url": "/shopping-list/static/css/main.65075bd6.chunk.css"
+  },
+  {
+    "revision": "05d080c48f21213de8c3",
+    "url": "/shopping-list/static/js/2.227b9bea.chunk.js"
+  },
+  {
+    "revision": "73fe2f17e0b5f3735cb6289f8d13e5a5",
+    "url": "/shopping-list/static/js/2.227b9bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81ada1cfa8641d4666a",
+    "url": "/shopping-list/static/js/main.9f0a2b18.chunk.js"
+  },
+  {
+    "revision": "ce3116cdc1602d5cc6ba",
+    "url": "/shopping-list/static/js/runtime-main.f9780ded.js"
+  }
+]);
