4fc67e7ff79206c3803a97f373cbf247147325bd
hangnguyen1199 <nthang1811@gmail.com>
2021-01-01 22:57:14 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./index.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Share from "./Share";
 import ShoppingList from "./shoppingList";
 function App() {
   return(
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <Switch>
-          <Route path="/shopping-list/" exact component={ShoppingList} />
+          <Route path="/" exact component={ShoppingList} />
           <Route path="/share" component={Share} />
         </Switch>
-      </Router>
+      </BrowserRouter>
     </div>
   )
 };
